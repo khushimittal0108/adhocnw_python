bf6dd400db3fc731ed3cfebb711e80520e5d0bb7
khushimittal0108 <khushimittal0858@gmail.com>
2019-06-14 11:39:41 +0530

    Update problem11sender.py

--- problem11sender.py
+++ problem11sender.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python2
 
 import socket
-import sys
 
 local_ip="127.0.0.1"
 local_port=4545
-time=100
 #you can assign any free port ,to see used udp ports use netstat -nulp
 
 s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-s.settimeout(time)
 while 5>2 :
 	message=raw_input("Sender :")
 	s.sendto(message,(local_ip,local_port))
